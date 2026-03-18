94e07980049ec36a7700f49dde5b789a72eb7df8
Minh <minhvip1982004@gmail.com>
2026-03-18 23:59:43 +0700

    Feat(Teacher): Implement Lesson management

--- src/main/java/com/thinkai/backend/repository/CourseRepository.java
+++ src/main/java/com/thinkai/backend/repository/CourseRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
@@ -28,5 +29,7 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
             @Param("priceMax") BigDecimal priceMax,
             Pageable pageable
     );
+
+    Optional<Course> findByIdAndInstructorId(Long id, Long instructorId);
 }
 
